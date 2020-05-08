d7ab0d9349c708d9fa1919b4a449e977c82b5ed0
EC2 Default User <ec2-user@ip-172-31-46-186.ap-south-1.compute.internal>
2020-05-08 20:38:50 +0000

    v2

--- gendata.sh
+++ gendata.sh
@@ -4,7 +4,7 @@
 set -x
 
 # define database connectivity
-_hostsrv="15.206.160.146"
+_hostsrv="15.206.170.123"
 _db="apksite"
 _db_user="admin"
 _db_password="toor"
@@ -30,29 +30,6 @@ for  apps in $(cat /tmp/appid.txt)
 
 	  mysql --host=$_hostsrv --user=$_db_user --password=$_db_password apksite -e "select CONCAT_WS('\n',title,descriptionHTML) from apkdetails where appid ='$apps'  INTO OUTFILE  '/tmp/$apps.txt';"
 
-#  _header_columns=`head -1 $_csv_directory/$_csv_file | tr ',' '\n' | sed 's/^"//' | sed 's/"$//' | sed 's/ /_/g'`
-#  _header_columns_string=`head -1 $_csv_directory/$_csv_file | sed 's/ /_/g' | sed 's/"//g'`
-
-  # ensure table exists
-#  mysql -h $_hostsrv -u $_db_user -p$_db_password $_db << eof
-#    CREATE TABLE IF NOT EXISTS \`$_table_name\` (
-#      id int(11) NOT NULL auto_increment,
-#      PRIMARY KEY  (id)
-#    ) ENGINE=MyISAM DEFAULT CHARSET=latin1
-#eof
-
-  # loop through header columns
-#  for _header in ${_header_columns[@]}
-#  do
-
-    # add column
-#    mysql -h $_hostsrv -u $_db_user -p$_db_password $_db --execute="alter table \`$_table_name\` add column \`$_header\` text"
-
-  done
-
-  # import csv into mysql
-#  mysqlimport --local --fields-enclosed-by='"' --fields-terminated-by=',' --lines-terminated-by="\n" --columns=$_header_columns_string -h $_hostsrv -u $_db_user -p$_db_password  $_db $_csv_directory/$_csv_file
-
-#done
+done
 exit
 
--- loader.sh
+++ loader.sh
@@ -4,7 +4,7 @@
 set -x
 
 # define database connectivity
-_hostsrv="15.206.160.146"
+_hostsrv="15.206.170.123"
 _db="apksite"
 _db_user="admin"
 _db_password="toor"
@@ -32,6 +32,8 @@ do
   _header_columns=`head -1 $_csv_directory/$_csv_file | tr ',' '\n' | sed 's/^"//' | sed 's/"$//' | sed 's/ /_/g'`
   _header_columns_string=`head -1 $_csv_directory/$_csv_file | sed 's/ /_/g' | sed 's/"//g'`
 
+sed -i '1d' $_csv_file #>${_csv_file}.tmp
+#mv ${_csv_file}.tmp $_csv_file
   # ensure table exists
 #  mysql -h $_hostsrv -u $_db_user -p$_db_password $_db << eof
 #    CREATE TABLE IF NOT EXISTS \`$_table_name\` (
@@ -40,17 +42,18 @@ do
 #    ) ENGINE=MyISAM DEFAULT CHARSET=latin1
 #eof
 
-  # loop through header columns
-#  for _header in ${_header_columns[@]}
-#  do
 
-    # add column
-#    mysql -h $_hostsrv -u $_db_user -p$_db_password $_db --execute="alter table \`$_table_name\` add column \`$_header\` text"
 
-#  done
+#Empty staging table before loading
+mysql --host=$_hostsrv --user=$_db_user --password=$_db_password apksite -e "TRUNCATE TABLE $_table_name ;"
+
+echo $?
 
   # import csv into mysql
   mysqlimport --local --fields-enclosed-by='"' --fields-terminated-by=',' --lines-terminated-by="\n" --columns=$_header_columns_string -h $_hostsrv -u $_db_user -p$_db_password  $_db $_csv_directory/$_csv_file
+
+
+mysql --host=$_hostsrv --user=$_db_user --password=$_db_password apksite -e "INSERT ignore INTO apkdetails SELECT * FROM stg_apkdetails WHERE stg_apkdetails.appid NOT IN (SELECT apkdetails.appid FROM apkdetails )"
 
 done
 exit
